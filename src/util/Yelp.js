0f67aa4e9834130461c766ff8f560bdb62a2421b
disruptif <oldophe.gaspard@gmail.com>
2017-11-07 22:56:47 +0100

    modif03

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -10,21 +10,29 @@ const Yelp = {
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {
       method: 'POST'}).then(response => {
         return response.json();
-      }).then(jsonResponse => {
+      })
+      .then(jsonResponse => {
           accessToken = jsonResponse.access_token;
-          return accessToken;
         })
   },
 
   search(term, location, sortBy) {
-    return Yelp.getAccessToken().then(() => {
-      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-        headers:{Authorization:`Bearer ${accessToken}`},
-      });
-    }).then(response => response.json()).then(jsonResponse => {
+    return Yelp.getAccessToken()
+    .then(() => {
+      return fetch(
+        `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {
+          headers:{
+            Authorization:`Bearer ${accessToken}`},
+        } // hearders close
+    ); // fetch close
+  }) // then close
+    .then(response => {
+      response.json();
+    })
+    .then(jsonResponse => {
       if (jsonResponse.businesses) {
-        return jsonResponse.businesses.map(business => {
-          return {
+        return jsonResponse.businesses.map(business => ({
             id: business.id,
             imageSrc: business.image_url,
             name: business.name,
@@ -32,14 +40,12 @@ const Yelp = {
             city: business.location.city,
             state: business.location.state,
             zipCode: business.location.zip_code,
-            category: business.categories,
+            category: business.categories[0].title,
             rating: business.rating,
             reviewCount: business.review_count
-          }
-        });
-      }
-    })
-  }
-}
+          }));
+        }
+      });
+    } // method search
 
 export default Yelp;

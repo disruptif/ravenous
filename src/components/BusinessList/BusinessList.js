afc3690551acd959d54b9575343cb3609f5e75b6
disruptif <oldophe.gaspard@gmail.com>
2017-11-07 21:25:41 +0100

    Ravenous

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business'; // import a Component
+import Yelp from '../../util/Yelp';
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        {
+          this.props.businesses.map(business => {
+            return <Business key={business.id} business={business} />;})
+        }
+      </div>
+    );
+  }
+}
+
+export default BusinessList;
